834b669a9b8a17cbcf365720f526eb52241b0615
harshshah181205 <harshshah123346@gmail.com>
2025-01-09 03:42:02 +0530

    Deployment changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://quiz-app-tau-ebon.vercel.app/api',
 });
 
 export const getQuizzes = () => api.get('/quizzes');
